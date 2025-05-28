c0cd8e8db3a7f024dc76b7242777a3209d2073e3
khoana <khoacoyotes.na@gmail.com>
2025-05-28 20:05:28 +0700

    feat(recipes): Add multi-category filter with improved UI layout
    - Implement checkbox-based category filter to support multiple selections
    - Update filter UI to use a clean, space-efficient layout
    - Align filter label and checkboxes in a single row
    - Use consistent typography with small text for better visual hierarchy
    - Remove unnecessary padding and container nesting
    - Maintain responsive behavior for smaller screens with flex-wrap

--- front-end/src/app/recipes/recipe-list/recipe-list.component.ts
+++ front-end/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { MealCategory } from '../../shared/enums/meal-category.enum';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -12,33 +13,62 @@ import { MealCategory } from '../../shared/enums/meal-category.enum';
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   @Input() recipes: Recipe[];
+  filteredRecipes: Recipe[];
   private _allRecipes: Recipe[];
   subscription: Subscription;
   categories = Object.values(MealCategory);
+  selectedCategories: Set<string> = new Set();
   
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) {
-  }
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     if (!this.recipes) {
-    this.subscription = this.recipeService.recipesChanged
-      .subscribe(
-        (recipes: Recipe[]) => {
-          this.recipes = recipes;
-        }
-      );
-    this.recipes = this.recipeService.getRecipes();
+      this.subscription = this.recipeService.recipesChanged
+        .subscribe(
+          (recipes: Recipe[]) => {
+            this._allRecipes = recipes;
+            this.recipes = recipes;
+            this.filterRecipes();
+          }
+        );
+      this._allRecipes = this.recipeService.getRecipes();
+      this.recipes = this._allRecipes;
+    } else {
+      this._allRecipes = this.recipes;
+    }
+    this.filterRecipes();
   }
-   // this._allRecipes = this.recipes.slice();
-}
+
+  onCategoryChange(category: string, isChecked: boolean) {
+    if (isChecked) {
+      this.selectedCategories.add(category);
+    } else {
+      this.selectedCategories.delete(category);
+    }
+    this.filterRecipes();
+  }
+
+  filterRecipes() {
+    if (this.selectedCategories.size === 0) {
+      this.recipes = this._allRecipes;
+    } else {
+      this.recipes = this._allRecipes.filter(recipe =>
+        this.selectedCategories.has(recipe.category)
+      );
+    }
+  }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
